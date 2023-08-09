8d26b12b0f7a3fd014a00816c51f44bfacc47b2c
Nuriye <nuriyeerdogan38@gmail.com>
2023-08-10 01:51:11 +0200

    Created 2 classes, player + weapon. I defined properties for eacc class, and implemented ToString methods. (omg i get it now??)

---
+++ LigesomBålhyttemenmedCSGO/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigesomBålhyttemenmedCSGO
+{
+    public class Player
+    {
+        // Properties
+
+        public int ID { get; set; }
+        public string Nickname { get; set; }
+        public int Health { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Player ID: " + ID + "\nNickname:" + Nickname + "\nHealth: " + Health + "\nKills: " + Kills + "\nDeaths: " + Deaths;
+        }
+
+    }
+
+    
+}
---
+++ LigesomBålhyttemenmedCSGO/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigesomBålhyttemenmedCSGO
+{
+    public class Weapon
+    {
+
+        // Properties
+        
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public int Ammo { get; set; }
+        public int ReloadTime { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Name: " + Name + "\nDamage: " + Damage + "\nAmmo: " + Ammo + "\nReload Time: " + ReloadTime;
+        }
+
+    }
+}
