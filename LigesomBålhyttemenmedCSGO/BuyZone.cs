dd80e8ea605cb8e7e8133858669bc5abb44f38b4
Nuriye <nuriyeerdogan38@gmail.com>
2023-08-10 23:30:31 +0200

    Created a new class called BuyZone, finished the project.

---
+++ LigesomBålhyttemenmedCSGO/BuyZone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigesomBålhyttemenmedCSGO
+{
+    public class BuyZone
+    {
+        // Properties
+
+        public string Location { get; set; }
+
+        // Implementing a list of available weapons in the buy zone
+        public List<Weapon> Weapons { get; set; }
+
+
+        // Implementing a constructor
+        public BuyZone(string location)
+        {
+            Location = location;
+            Weapons = new List<Weapon>();
+        }
+
+        // Implementing a menthod, printing deatails of weapons available in the buy zone.
+        public void PrintBuyZoneWeapons()
+        {
+            Console.WriteLine($"Weapons available at {Location}");
+            foreach (Weapon weapon in Weapons)
+            {
+                Console.WriteLine(weapon.ToString());
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+
+   
+  
+}
--- LigesomBålhyttemenmedCSGO/Program.cs
+++ LigesomBålhyttemenmedCSGO/Program.cs
@@ -9,7 +9,7 @@
             Player player2 = new Player(002, "kneeyaz", 100, 43, 25);
             Player player3 = new Player(003, "nukeSZN", 56, 54, 05);
 
-            // printing it to the console
+            //  printing it to the console
             Console.WriteLine(player1);
             Console.WriteLine(player2);
             Console.WriteLine(player3);
@@ -26,6 +26,19 @@
             Console.WriteLine(weapon3);
 
 
+            // creating instances of BuyZone
+            BuyZone buyzone1 = new BuyZone("T-Side");
+            BuyZone buyzone2 = new BuyZone("CT-Side");
+
+            // Adding weapons to the buy zones
+            buyzone1.Weapons.Add(weapon1);
+            buyzone1.Weapons.Add(weapon2);
+            buyzone2.Weapons.Add(weapon3);
+
+
+            // printing it out to the console
+            buyzone1.PrintBuyZoneWeapons();
+            buyzone2.PrintBuyZoneWeapons();
 
         }
     }
